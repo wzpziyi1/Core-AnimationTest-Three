c901362317a75574a3d4c7ef20ba841bc873e10f
wzpziyi1 <1204473090@qq.com>
2016-02-21 12:22:31 +0800

    first commit

---
+++ 彩票转盘/ZYWheelView.h
@@ -0,0 +1,13 @@
+//
+//  ZYWheelView.h
+//  彩票转盘
+//
+//  Created by 王志盼 on 16/2/15.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYWheelView : UIView
+
+@end
---
+++ 彩票转盘/ZYWheelView.m
@@ -0,0 +1,21 @@
+//
+//  ZYWheelView.m
+//  彩票转盘
+//
+//  Created by 王志盼 on 16/2/15.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import "ZYWheelView.h"
+
+@implementation ZYWheelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
